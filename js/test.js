534e828a844ce77425a4aae7fcc7441c7d1b4499
Timothy Lim <timothy.t.lim@gmail.com>
2014-03-09 19:16:08 -0400

    turn js test

--- js/test.js
+++ js/test.js
@@ -1,3 +1,10 @@
 console.log('welcome to my page!');
 
+
 console.log('               		 ***                  ***\n                    *****                *****\n                    *****                *****\n                     ***                  ***\n          ***                                        ***\n           ***                                      ***\n            ***                                    ***\n             ***                                  ***\n               ***                              ***\n                 ***                          ***\n                   ***                      ***\n                      **********************\n                         ****************\n')
+
+	$("#flipbook").turn({
+		width: 400,
+		height: 300,
+		autoCenter: true
+	});
