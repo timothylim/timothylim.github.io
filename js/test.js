b27bf9fa5a117b72729331739d97ce8ff4b67647
Timothy Lim <timothy.t.lim@gmail.com>
2014-03-09 19:22:23 -0400

    remove turn js

--- js/test.js
+++ js/test.js
@@ -3,8 +3,3 @@ console.log('welcome to my page!');
 
 console.log('               		 ***                  ***\n                    *****                *****\n                    *****                *****\n                     ***                  ***\n          ***                                        ***\n           ***                                      ***\n            ***                                    ***\n             ***                                  ***\n               ***                              ***\n                 ***                          ***\n                   ***                      ***\n                      **********************\n                         ****************\n')
 
-	$("#flipbook").turn({
-		width: 400,
-		height: 300,
-		autoCenter: true
-	});
