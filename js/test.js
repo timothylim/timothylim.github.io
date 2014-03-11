05812eafb2551fea6d72fc879cc80383f884a68d
Timothy Lim <timothy.t.lim@gmail.com>
2014-03-10 22:28:12 -0400

    document ready

--- js/test.js
+++ js/test.js
@@ -2,17 +2,18 @@ console.log('welcome to my page!');
 
 
 console.log('               		 ***                  ***\n                    *****                *****\n                    *****                *****\n                     ***                  ***\n          ***                                        ***\n           ***                                      ***\n            ***                                    ***\n             ***                                  ***\n               ***                              ***\n                 ***                          ***\n                   ***                      ***\n                      **********************\n                         ****************\n')
+$(document).ready(function(){
+	$('#startbutton').click(function(){
+		var input = $('#speedinput').val();
+		input = input.split(' ');
+		$('#speedinput').val('');
+		var i = input.length;
+		while(i){
+			window.setInterval(function(){
+				$('#speedoutput').html(input[i]);
+				i--;
+			}, 1000)
+		}
 
-$('#startbutton').click(function(){
-	var input = $('#speedinput').val();
-	input = input.split(' ');
-	$('#speedinput').val('');
-	var i = input.length;
-	while(i){
-		window.setInterval(function(){
-			$('#speedoutput').html(input[i]);
-			i--;
-		}, 1000)
-	}
-
+	});
 });
