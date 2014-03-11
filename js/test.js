e10c1a55105e4b3698f0fd240b38193d7b16fd32
Timothy Lim <timothy.t.lim@gmail.com>
2014-03-10 23:34:04 -0400

    wpm

--- js/test.js
+++ js/test.js
@@ -4,16 +4,18 @@ console.log('welcome to my page!');
 console.log('               		 ***                  ***\n                    *****                *****\n                    *****                *****\n                     ***                  ***\n          ***                                        ***\n           ***                                      ***\n            ***                                    ***\n             ***                                  ***\n               ***                              ***\n                 ***                          ***\n                   ***                      ***\n                      **********************\n                         ****************\n')
 $(document).ready(function(){
 	$('#startbutton').click(function(){
-		var input = $('#speedinput').val();
+		var input = $('#inputText').val();
 		input = input.split(' ');
-		$('#speedinput').val('');
+		$('#inputText').val('');
 		var i = input.length + 1;
+		var wpm = $('#inputSpeed').val() > 0 ? $('#inputSpeed').val() : 60;
+		wpm = 60/wpm *1000;
 		var interval = setInterval(function(){
 			var val = input.splice(0,1);
 			$('#outputLabel').html(val);
-		}, 1000)
+		}, wpm)
 		setTimeout(function(){
 			clearInterval(interval);
-		}, i * 1000 );
+		}, i * wpm );
 	});
 });
