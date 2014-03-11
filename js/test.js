779f2900770385b2976a4c670c26c7d9f5f5658b
Timothy Lim <timothy.t.lim@gmail.com>
2014-03-11 00:19:23 -0400

    double click handling

--- js/test.js
+++ js/test.js
@@ -7,26 +7,31 @@ $(document).ready(function(){
 	var storedInput;
 	$('#startbutton').click(function(){
 		//check for stopped, start becomes restart
-		if(!$('#startbutton').hasClass('stopped') && !$('#inputText').val() == ''){
-			storedInput = $('#inputText').val();
+		if(!$('#startbutton').hasClass('clicked')){
+			$('#startbutton').addClass('clicked');
+			if(!$('#startbutton').hasClass('stopped') && !$('#inputText').val() == ''){
+				storedInput = $('#inputText').val();
+			}
+			input = storedInput.split(' ');
+			$('#inputText').val('');
+			var i = input.length + 1;
+			var wpm = $('#inputSpeed').val() > 0 ? $('#inputSpeed').val() : 250;
+			wpm = 60/wpm *1000;
+			interval = setInterval(function(){
+				var val = input.splice(0,1);
+				$('#outputLabel').html(val);
+			}, wpm)
+			setTimeout(function(){
+				clearInterval(interval);
+				$('#startbutton').val('start')
+				$('#startbutton').removeClass('clicked');
+			}, i * wpm );
 		}
-		input = storedInput.split(' ');
-		$('#inputText').val('');
-		var i = input.length + 1;
-		var wpm = $('#inputSpeed').val() > 0 ? $('#inputSpeed').val() : 250;
-		wpm = 60/wpm *1000;
-		interval = setInterval(function(){
-			var val = input.splice(0,1);
-			$('#outputLabel').html(val);
-		}, wpm)
-		setTimeout(function(){
-			clearInterval(interval);
-			$('#startbutton').val('start')
-		}, i * wpm );
 	});
 	$('#stopbutton').click(function(){
 		clearInterval(interval);
 		$('#startbutton').addClass('stopped');
-		$('#startbutton').val('restart')
+		$('#startbutton').val('restart');
+		$('#startbutton').removeClass('clicked');
 	})
 });
