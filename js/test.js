5daa660adb431f91e6ffd16d73dec223494dcd1b
Timothy Lim <timothy.t.lim@gmail.com>
2014-03-20 22:11:13 -0400

    y axis

--- js/test.js
+++ js/test.js
@@ -173,7 +173,7 @@ $(function(){
 		// X scale will fit all values from data[] within pixels 0-w
 		var x = d3.scale.linear().domain([.25, 5.5]).range([0, w]);
 		// Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-		var y = d3.scale.linear().domain([indices]).range([h, 0]);
+		var y = d3.scale.linear().domain([0,200]).range([h, 0]);
 			// automatically determining max range can work something like this
 			// var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
 
