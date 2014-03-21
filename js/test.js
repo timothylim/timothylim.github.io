3cdd1db2c49fcedfc3e1f893ec347e436488094a
Timothy Lim <timothy.t.lim@gmail.com>
2014-03-21 18:33:13 -0400

    color line and text

--- js/test.js
+++ js/test.js
@@ -195,35 +195,34 @@ $(function(){
 			})
 
 			// Add an SVG element with the desired dimensions and margin.
-			var graph = d3.select("#graph").append("svg:svg")
-			      .attr("width", w + m[1] + m[3])
-			      .attr("height", h + m[0] + m[2])
-			    .append("svg:g")
-			      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+		var graph = d3.select("#graph").append("svg:svg")
+		      .attr("width", w + m[1] + m[3])
+		      .attr("height", h + m[0] + m[2])
+  		      .append("svg:g")
+		      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
 			// create yAxis
-			var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+		var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
 			// Add the x-axis.
-			graph.append("svg:g")
-			      .attr("class", "x axis")
-			      .attr("transform", "translate(0," + h + ")")
-			      .call(xAxis);
+		graph.append("svg:g")
+		      .attr("class", "x axis")
+		      .attr("transform", "translate(0," + h + ")")
+		      .call(xAxis);
 
 
 			// create left yAxis
-			var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+		var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
 			// Add the y-axis to the left
-			graph.append("svg:g")
-			      .attr("class", "y axis")
-			      .attr("transform", "translate(-25,0)")
-			      .call(yAxisLeft);
+		graph.append("svg:g")
+		      .attr("class", "y axis")
+		      .attr("transform", "translate(-25,0)")
+		      .call(yAxisLeft);
 			
   			// Add the line by appending an svg:path element with the data line we created above
 			// do this AFTER the axes above so that the line is above the tick-lines
-  			graph.append("svg:path").attr("d", line(twelveOz));
-			graph.append("svg:path").attr("d", line(sevenfiveOz));
-
-
+		graph.append("svg:path").attr("d", line(twelveOz));
+		graph.append("svg:path").attr("d", line(sevenfiveOz));
+		$('path').last().css('stroke', 'red')
 
 
 });
