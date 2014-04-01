a294126d04bc91dd9140831e2aa39cac6e2288b0
Timothy Lim <timothy.t.lim@gmail.com>
2014-03-31 20:37:03 -0400

    analytics

--- js/test.js
+++ js/test.js
@@ -451,5 +451,14 @@ $(function(){
         console.log('not a valid wall neighbor');
     	}
 	}
-	//$('#applySize').click();
+	$('#applySize').click();
+
+	//analytics
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-49593509-1', 'timothylim.github.io');
+	  ga('send', 'pageview');
 });
