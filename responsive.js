f5a1b8975b95dccb7624568c9acf104b6a0a1388
Timothy Lim <timothy.t.lim@gmail.com>
2014-07-18 13:28:12 -0400

    forgot + sign

--- responsive.js
+++ responsive.js
@@ -13,7 +13,7 @@
 
     $('#dpi').text("dpi: " + dpi_x + "x" +dpi_y);
 
-    $('#window').text("window dimensions: " + $(window).height() "x" + $(window).width());
+    $('#window').text("window dimensions: " + $(window).height() +"x" + $(window).width());
 
 })
 
