7a10d0ef89a2a0c68a1ff768487ab0fcd96078f3
Timothy Lim <timothy.t.lim@gmail.com>
2014-07-18 06:42:17 -0400

    added responsive test

---
+++ responsive.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    $('#pixels').text("pixel resolution: " + window.screen.availHeight + "x" + window.screen.availWidth);
+
+    $('#ratio').text("pixel ratio: " + window.devicePixelRatio);
+
+    $('#orientation').text("orientation: " + window.screen.orientation + "");
+
+
+    var devicePixelRatio = window.devicePixelRatio || 1;
+    dpi_x = document.getElementById('testdiv').offsetWidth * devicePixel,gdgfRatio;
+    dpi_y = document.getElementById('testdiv').offsetHeight * devicePixelRatio;
+
+    $('#dpi').text("dpi: " + dpi_x + "x" +dpi_y);
+
+
+})
+
