05607b5259528b13e80171eab48f67b867b48890
Timothy Lim <timothy.t.lim@gmail.com>
2014-07-18 13:30:54 -0400

    on resize

--- responsive.js
+++ responsive.js
@@ -1,19 +1,18 @@
 ﻿$(function () {
 
-    $('#pixels').text("pixel resolution: " + window.screen.availHeight + "x" + window.screen.availWidth);
-
-    $('#ratio').text("pixel ratio: " + window.devicePixelRatio);
-
-    $('#orientation').text("orientation: " + window.screen.orientation + "");
-
-
     var devicePixelRatio = window.devicePixelRatio || 1;
     dpi_x = document.getElementById('testdiv').offsetWidth * devicePixelRatio;
     dpi_y = document.getElementById('testdiv').offsetHeight * devicePixelRatio;
 
     $('#dpi').text("dpi: " + dpi_x + "x" +dpi_y);
+    $(window).resize(function(){
+    	$('#pixels').text("pixel resolution: " + window.screen.availHeight + "x" + window.screen.availWidth);
 
-    $('#window').text("window dimensions: " + $(window).height() +"x" + $(window).width());
+    	$('#ratio').text("pixel ratio: " + window.devicePixelRatio);
 
+    	$('#orientation').text("orientation: " + window.screen.orientation + "");
+
+    	$('#window').text("window dimensions: " + $(window).height() +"x" + $(window).width());
+	}
 })
 
