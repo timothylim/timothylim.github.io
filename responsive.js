53e7ba75d94d2d919fa84bcc389fac67619582c8
Timothy Lim <timothy.t.lim@gmail.com>
2014-07-18 13:34:47 -0400

    added back static values

--- responsive.js
+++ responsive.js
@@ -1,5 +1,14 @@
 ﻿$(function () {
 
+    	$('#pixels').text("pixel resolution: " + window.screen.availHeight + "x" + window.screen.availWidth);
+
+    	$('#ratio').text("pixel ratio: " + window.devicePixelRatio);
+
+    	$('#orientation').text("orientation: " + window.screen.orientation + "");
+
+    	$('#window').text("window dimensions: " + $(window).height() +"x" + $(window).width());
+
+
     var devicePixelRatio = window.devicePixelRatio || 1;
     dpi_x = document.getElementById('testdiv').offsetWidth * devicePixelRatio;
     dpi_y = document.getElementById('testdiv').offsetHeight * devicePixelRatio;
